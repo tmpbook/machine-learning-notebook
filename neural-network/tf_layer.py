00c1f450e165bd382833f4c21015ae8421e0dfc1
tmpbook <nauy2011@126.com>
2017-11-04 20:34:12 +0800

    update the visualize

--- neural-network/tf_layer.py
+++ neural-network/tf_layer.py
@@ -32,10 +32,21 @@ init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
 
-losses = []
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+ax.scatter(x_data, y_data)
+plt.ion()
+plt.show()
+
 for i in range(1000):
     sess.run(train_step, feed_dict={xs: x_data, ys: y_data})
-    current_loss = sess.run(loss, feed_dict={xs: x_data, ys: y_data})
-    losses.append(current_loss)
-plt.plot(losses)
-plt.show()
+    if i % 50 == 0:
+        # to visualize the result and improvement
+        try:
+            ax.lines.remove(lines[0])
+        except Exception:
+            pass
+        prediction_value = sess.run(prediction, feed_dict={xs: x_data})
+        # plot the prediction
+        lines = ax.plot(x_data, prediction_value, 'r-', lw=5)
+        plt.pause(0.5)
